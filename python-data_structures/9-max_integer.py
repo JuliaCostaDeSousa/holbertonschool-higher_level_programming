3acf0e0e8bcb71ac6736c553cac1b2fefc645d89
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-05-13 16:11:59 +0200

    instructions

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list or len(my_list) == 0:
+        return None
+    else:
+         my_list.sort(reverse = True)
+    return my_list[0]
