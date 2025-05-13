582199bea06036198cd120c7d2e9a447dd85235e
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-05-13 12:26:52 +0200

    correction for chek out of range

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if not my_list or idx > len(my_list) or my_list[idx] < 0:
+    if not my_list or idx >= len(my_list) or my_list[idx] < 0:
         return None
     else:
         return my_list[idx]
