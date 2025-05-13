6ee73c16aa0032758406031502e5416f669d4a35
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-05-13 16:13:09 +0200

    correction for pycodestyle

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,5 +3,5 @@ def max_integer(my_list=[]):
     if not my_list or len(my_list) == 0:
         return None
     else:
-         my_list.sort(reverse = True)
+        my_list.sort(reverse=True)
     return my_list[0]
