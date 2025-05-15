aa72ca40e67e41fb7c412f2b62191bc41c375161
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-05-15 12:15:46 +0200

    delete test in .py

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module provides a function that divides all elements of a matrix.
+
+It handles a list of a list integers or floats.
+If inputs are not int or float, it raises a TypeError.
+
+You are not allowed to import any module.
+"""
+
+
+def matrix_divided(matrix, div):
