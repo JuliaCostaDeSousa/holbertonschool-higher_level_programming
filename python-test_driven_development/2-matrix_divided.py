92142bc7affaacc28c504489355dc260eda991eb
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-05-15 13:31:57 +0200

    Adjust check for list of list and tests for just lists

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -15,7 +15,7 @@ def matrix_divided(matrix, div):
 
     >>> matrix_divided([[2, 2], [4, 8]], 2)
     [[1.0, 1.0], [2.0, 4.0]]
-    >>> matrix_divided([1, 2, 3], [4, 5, 6], 3)
+    >>> matrix_divided([[1, 2, 3], [4, 5, 6]], 3)
     [[0.33, 0.67, 1.0], [1.33, 1.67, 2.0]]
     """
 
@@ -23,6 +23,9 @@ def matrix_divided(matrix, div):
         raise TypeError("matrix must be a matrix "
                         "(list of lists) of integers/floats")
     for row in matrix:
+        if not isinstance(row, (list)):
+            raise TypeError("matrix must be a matrix "
+                        "(list of lists) of integers/floats")
         if len(row) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
         for x in row:
@@ -33,9 +36,11 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
+    
     new_matrix = []
-
     for row in matrix:
+        new_row = []
         for x in row:
-            new_matrix[row, x] = round(matrix[row, x] / div, 2)
+            new_row.append(round(x / div, 2))
+        new_matrix.append(new_row)
     return new_matrix
