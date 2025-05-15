5625a7c442254dbe898193fa7490c720262d19d3
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-05-15 13:32:55 +0200

    correct pycodestyle

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -25,7 +25,7 @@ def matrix_divided(matrix, div):
     for row in matrix:
         if not isinstance(row, (list)):
             raise TypeError("matrix must be a matrix "
-                        "(list of lists) of integers/floats")
+                            "(list of lists) of integers/floats")
         if len(row) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
         for x in row:
@@ -36,7 +36,7 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    
+
     new_matrix = []
     for row in matrix:
         new_row = []
