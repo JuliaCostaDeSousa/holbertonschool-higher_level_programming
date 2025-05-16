562ce28525aae15a7b70ff5f185ce1ecb2c5fdce
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-05-16 10:34:08 +0200

    wrinting instrutions

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module gives a function that prints a text 
+with 2 new lines after each of these characters: ., ? and :
+
+It handles a string.
+If text is not a string, it raises a TypeError.
+
+You are not allowed to import any module.
+"""
+
+
+def print_square(size):
+    """
+    Prints a text 
+    with 2 new lines after each of these characters: ., ? and :
+    
+    
+
+    """
+
+    
