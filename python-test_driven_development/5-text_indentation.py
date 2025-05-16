3c31ab18090311f2b4c60dc014003f080135ad78
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-05-16 13:32:17 +0200

    writing instructions

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module gives a function that prints a text 
+This module gives a function that prints a text
 with 2 new lines after each of these characters: ., ? and :
 
 It handles a string.
@@ -10,13 +10,30 @@ You are not allowed to import any module.
 """
 
 
-def print_square(size):
+def text_indentation(text):
     """
-    Prints a text 
+    Prints a text
     with 2 new lines after each of these characters: ., ? and :
-    
-    
 
+    >>> text_indentation(".txt?")
+
+    txt
+
+    >>> text_indentation("txt")
+    txt
     """
 
-    
+    if not isinstance(text, (str)):
+        raise TypeError("text must be a string")
+
+    text_line = []
+    for character in text:
+        text_line.append(character)
+        print(character, end="")
+        if character in ['.', '?', ':']:
+            if text_line[0] == " ":
+                line = line[1:]
+            if text_line[-1] == " ":
+                line = line[:-1]
+            print("\n")
+            line = []
