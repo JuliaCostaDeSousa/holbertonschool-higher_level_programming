6acaa5bbd2e7fd43f5949b706263b15df5c7ecff
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-05-18 16:28:05 +0200

    write instructions

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -4,15 +4,27 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
 
     def test_max(self):
-        max_val = max_integer([0, 500, 200, -650])
-        self.assertEqual(max_val, 500, "The max is wrong")
+        self.assertEqual(max_integer([0, 500, 200, -650]), 500, "Max is wrong")
 
-    def test_type(self):
-        self.assertIsInstance(a, b)	
+    def test_empty_list(self):
+        self.assertIsNone(max_integer([]), "Should return None for empty list")
 
+    def test_single_element(self):
+        self.assertEqual(max_integer([42]), 42)
+
+    def test_all_negative(self):
+        self.assertEqual(max_integer([-10, -20, -3, -40]), -3)
+
+    def test_floats(self):
+        self.assertEqual(max_integer([1.5, 2.7, 0.2]), 2.7)
+
+    def test_non_numeric_elements(self):
+        with self.assertRaises(TypeError):
+            max_integer([1, "two", 3])
 
 
 if __name__ == '__main__':
