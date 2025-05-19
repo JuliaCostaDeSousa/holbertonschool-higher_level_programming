93be9123f22674c6c6115f04ed6359683c78f006
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-05-19 15:36:05 +0200

    writing instructions for 0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+module 0-square.py
+
+This module defines an empty Square class.
+"""
+
+
+class Square:
+    """
+    Empty class that defines a square.
+
+    This class will be updated later.
+    """
+
+    pass
