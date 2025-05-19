2c63f3000affc0bd647c9b99a3313ac1438ed968
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-05-19 16:00:21 +0200

    writing instructions for 2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+module 2-square.py
+
+This module defines a Square class to define a square by its size.
+"""
+
+
+class Square:
+    """
+    Class that defines a square by its size (private attribute)
+
+    """
+
+    def __init__(self, size = 0):
+        """
+        Initialize a new square by its size
+
+        Args:
+            size (int) : size of the square
+        """
+
+        if not isinstance(size, (int)):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        
+        self.__size = size
