f54e3c81a04e2ca49756a7b1e1dde14a64bc5dcd
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-05-20 16:48:56 +0200

    writing instructions for 0-rectangle.py

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+module 0-rectangle.py
+
+This module provides an empty Rectangle class
+"""
+
+
+class Rectangle:
+    """
+    Empty class that defines a rectangle.
+
+    This class will be updated later.
+    """
+
+    pass
