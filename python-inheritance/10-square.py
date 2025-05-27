6d92f046c3c651691a116025c4155822b89be6d3
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-05-27 16:51:33 +0200

    exo 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+This module provides the Square class
+"""
+
+
+class Square(Rectangle):
+    """
+    Class that inherits from Rectangle
+    """
+
+    def __init__(self, size):
+        """
+        Initializes an instance with size
+        """
+
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        """
+        Returns the rectangle description
+        """
+
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+
+    def area(self):
+        """
+        Returns the square area
+        """
+
+        return self.__size * self.__size
