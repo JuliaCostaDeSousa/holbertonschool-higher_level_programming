4f6b80e072ae8ae372571411a33ff7a7206ccce4
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-05-29 16:33:48 +0200

    writing instructions for task 05

--- python-abc/main_05_dragon.py
+++ python-abc/main_05_dragon.py
@@ -5,3 +5,6 @@ dragon = Dragon()
 dragon.swim()  # Outputs: The creature swims!
 dragon.fly()   # Outputs: The creature flies!
 dragon.roar()  # Outputs: The dragon roars!
+dragon.flame_throwing()  # Outputs: The dragon is throwing flames!
+dragon.sleep()  # Outputs: The dragon is sleeping on its treasure!
+dragon.sing()  # Outputs: The dragon is singing Through the Fire and Flames!
