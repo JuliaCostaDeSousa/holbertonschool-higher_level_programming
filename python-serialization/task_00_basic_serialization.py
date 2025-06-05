3f422ea3ab8e4a53db3adacc06b24110f6d76e9c
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-06-05 21:59:44 +0200

    writing instructions

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module task_00_basic_serialization
+"""
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """
+    Serialize data and save it to JSON file
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(json.dumps(my_obj))
+
+    pass
+
+def load_and_deserialize(filename):
+    """
+    
+    """
+
+    with open(filename, 'r') as openfile:
+        return json.load(openfile)
+    
+    pass
