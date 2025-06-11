3891103b9d2baf05e51df7d5f3513eb0366eb1fe
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-06-11 18:59:56 +0200

    writing instructions

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Module task_02_requests
+"""
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    """
+    Fetches all post from JSONPlaceholder
+    """
+
+    r = requests.get('https://jsonplaceholder.typicode.com/todos')
+    print("Status Code: {}".format(r.status_code))
+    if r.status_code == 200:
+        response_json = r.json()
+        for post in response_json:
+            print(post["title"])
+    else:
+        print("Request error")
+
+
+def fetch_and_save_posts():
+    """
+    Fetches all post from JSONPlaceholder
+    """
+
+    r = requests.get('https://jsonplaceholder.typicode.com/todos')
+    print("Status Code: {}".format(r.status_code))
+    if r.status_code == 200:
+        response_json = r.json()
+        with open('posts.csv', 'w', newline='') as csvfile:
+            csv_w = csv.writer(csvfile, delimiter=',')
+            csv_w.writerow(["id", "title", "body"])
+            for line in response_json:
+                csv_w.writerow([line["id"], line["title"], line["completed"]])
+    else:
+        print("Request error")
