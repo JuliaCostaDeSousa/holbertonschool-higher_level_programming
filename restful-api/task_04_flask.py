c4fef14b7a98d13676a9f41c1f8048c4633e3271
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-06-12 10:23:15 +0200

    finishing instructions

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -7,6 +7,8 @@ from flask import jsonify
 from flask import request
 
 app = Flask(__name__)
+users = {}
+
 
 @app.route("/")
 def home():
@@ -14,14 +16,16 @@ def home():
     Handle root URL "/"
     """
 
-    return "Welcome to the Flask API!" 
+    return "Welcome to the Flask API!"
+
 
 @app.route("/data")
 def data():
     """
     Handle root URL "/data"
     """
-    return jsonify({"username" : username})
+
+    return jsonify(list(users.keys())), 200
 
 
 @app.route("/status")
@@ -32,26 +36,31 @@ def status():
 
     return "OK"
 
+
 @app.route("/users/<username>")
-def username(username):
+def username_route(username):
     """
     Handle root URL "/users/<username>"
     """
-    try:
-        return jsonify({"username" : username})
-    except Exception as e:
-        return {"error": "User not found"}
+    if username in users:
+        return jsonify(users[username])
+    return jsonify({"error": "User not found"}), 400
 
 
-@app.route("/add_user")
+@app.route("/add_user", methods=["POST"])
 def add_user():
     """
     Handle root URL "/add_user"
     """
 
-    with app.test_request_context('/add_user', method='POST'):
-        assert request.path == '/add_user'
-        assert request.method == 'POST'
-        assert request.status_code == '201'
+    data = request.get_json()
+    print("Reçu :", data)
+    if not data or "username" not in data:
+        return jsonify({"error": "Username is required"}), 400
+    username = data["username"]
+    users[username] = data
+    return jsonify({"message": "User added", "user": data}), 201
 
-if __name__ == "__main__": app.run()
+
+if __name__ == "__main__":
+    app.run()
