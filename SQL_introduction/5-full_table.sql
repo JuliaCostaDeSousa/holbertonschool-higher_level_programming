561d1508f36d75705bd6c3646257d28c3984de30
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-06-23 16:43:11 +0200

    writing instructions for all tasks

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,2 @@
+-- Prints description of a table in my MySQL server
+SHOW CREATE TABLE first_table;
---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,2 @@
+-- Lists all rows of a table from a database in my MySQL
+SELECT * FROM first_table;
