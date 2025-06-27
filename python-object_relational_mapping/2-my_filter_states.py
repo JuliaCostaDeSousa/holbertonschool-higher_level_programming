40da64fa5dbec103edf94a803d0864e609f708a4
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-06-27 09:21:20 +0200

    change format query

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -19,8 +19,9 @@ if __name__ == "__main__":
     )
     state_name = sys.argv[4]
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"\
-        .format(state_name)
+    query = "SELECT * FROM states WHERE name = '{}'" \
+            "ORDER BY id ASC"\
+            .format(state_name)
     cur.execute(query)
     rows = cur.fetchall()
     for row in rows:
