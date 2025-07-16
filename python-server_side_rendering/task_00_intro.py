c4169b0d1cba099e213ea4299b2dd077bc72972f
JuliaCostaDeSousa <costadesousa.julia@gmail.com>
2025-07-17 00:52:08 +0200

    task_00_intro.py

---
+++ python-server_side_rendering/task_00_intro.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+Module generate_invitations
+"""
+import os
+
+
+def generate_invitations(template, attendees):
+    """
+    Generates personalized invitation files from a template
+    """
+
+    if not isinstance(template, str):
+        raise TypeError('{}'.format(type(template)))
+    if not isinstance(attendees, list):
+        raise TypeError('{}'.format(type(attendees)))
+    else:
+        for i in range(len(attendees)):
+            if not isinstance(attendees[i], dict):
+                raise TypeError('{}'.format(type(attendees[i])))
+
+    if not template or template == "":
+        raise ValueError('Template is empty, no output files generated.')
+
+    if not attendees or attendees == []:
+        raise ValueError('No data provided, no output files generated.')
+
+    replace_fields = ['name', 'event_title', 'event_date', 'event_location']
+
+    for i in range(len(attendees)):
+        try:
+            filename = 'output_{}.txt'.format(i + 1)
+            with open(filename, 'w') as file:
+                invitation = template
+                for j in range(len(replace_fields)):
+                    placeholder = "{" + replace_fields[j] + "}"
+                    if attendees[i][replace_fields[j]] is None:
+                        if replace_fields[j] == "event_date":
+                            invitation = invitation.replace(
+                                placeholder, "event_date:N/A")
+                        else:
+                            invitation = invitation.replace(placeholder, "N/A")
+                    else:
+                        invitation = invitation.replace(
+                            placeholder, attendees[i][replace_fields[j]])
+                file.write(invitation)
+        except Exception as e:
+            print("Error generating invitation for attendee\
+                   #{}: {}".format(i + 1, e))
